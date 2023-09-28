d32aceb8bc32214811d7766636fd3045ef68768e
leobar37 <leonbarbozaelmerjoselito@gmail.com>
2023-09-28 01:12:24 -0500

    feat: add products list design

--- src/components/products/GridProducts.tsx
+++ src/components/products/GridProducts.tsx
@@ -4,17 +4,32 @@ import { productsAtom } from "~/lib/shopify/atoms";
 import { FC } from "react";
 import { useHydrateAtoms } from "jotai/utils";
 import { CardProduct } from "./ProductHomeCard";
+import { useAtomValue } from "jotai";
+
 type GridProductsProps = {
   products: Product[];
 };
-export const GridProducts: FC<GridProductsProps> = ({ products }) => {
-  useHydrateAtoms([[productsAtom, products]]);
+
+export const GridProducts: FC<GridProductsProps> = ({
+  products: productsFromServer,
+}) => {
+  useHydrateAtoms([[productsAtom, productsFromServer]]);
+
+  const products = useAtomValue(productsAtom);
 
   return (
-    <div className="grid grid-cols-3 max-w-7xl m-auto">
-      {products.map((pr) => (
-        <CardProduct product={pr} />
-      ))}
+    <div className="text-black py-8 max-w-7xl mx-auto">
+      <div className="text-center">
+        <h2 className="text-xl">Nuestra selección de cafés de alta calidad</h2>
+        <h3 className="text-5xl font-semibold">
+          Todos nuestros cafés cuentan su propia historia
+        </h3>
+      </div>
+      <div className="grid mt-5 place-content-center grid-cols-3 max-w-7xl m-auto">
+        {products.map((pr) => (
+          <CardProduct product={pr} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/products/ProductHomeCard.tsx
+++ src/components/products/ProductHomeCard.tsx
@@ -1,23 +1,54 @@
 import NextImage from "next/image";
 import { FC } from "react";
 import { Product } from "~/lib/shopify";
+import { useHover } from "@uidotdev/usehooks";
+import { EyeIcon, BagIcon } from "~/components/icons";
+const MAP_CURREMCY = new Map([
+  ["USD", "$"],
+  ["PEN", "S/."],
+]);
 
 export const CardProduct: FC<{
   product: Product;
 }> = ({ product }) => {
   const image = product.images?.[0];
+  const [ref, hovering] = useHover();
+  const rawPrice = product.variants[0].price;
+  const priceToCompare = product.variants[0].compareAtPrice;
+
+  const resolvePrice = (price: number) => {
+    return `${MAP_CURREMCY.get(rawPrice.currencyCode)}${price}`;
+  };
+
   return (
-    <div className="p-3 flex flex-col max-w-fit items-center w-64 bg-white shadow-lg  rounded-md">
-      <p className="font-semibold text-black">{product.title}</p>
+    <div
+      ref={ref as any}
+      className="p-3 cursor-pointer flex flex-col border-2 border-black max-w-max items-center border-none relative"
+    >
+      {hovering && (
+        <div className="flex flex-col gap-y-2 absolute right-0  bottom-16">
+          <div className="wrap-icon">
+            <EyeIcon />
+          </div>
+          <div className="wrap-icon">
+            <BagIcon />
+          </div>
+        </div>
+      )}
       <div>
         {image && (
           <NextImage
             alt={image.altText ?? ""}
             src={image.src}
-            width={120}
-            height={120}
+            width={370}
+            height={400}
           />
         )}
+      </div>
+      <p className="font-dmSans text-black font-normal">{product.title}</p>
+      <div className="flex space-x-2 text-lg">
+        <p>{resolvePrice(rawPrice.amount)}</p>{" "}
+        {priceToCompare && <b>{resolvePrice(priceToCompare.amount)}</b>}
       </div>
     </div>
   );
