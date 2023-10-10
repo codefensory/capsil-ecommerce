1cd5ad9919c5648712947b38e9e6e20c495f566a
leobar37 <leonbarbozaelmerjoselito@gmail.com>
2023-10-10 11:30:36 -0500

    fix: grid products mobile

--- src/components/products/GridProducts.tsx
+++ src/components/products/GridProducts.tsx
@@ -12,12 +12,14 @@ type Props = {
 export const GridProducts: FC<Props> = ({ title, smallTitle }) => {
   const products = useAtomValue(productsAtom);
   return (
-    <div className="text-black py-8 max-w-7xl mx-auto">
+    <div className="text-black py-8 toTable:py-5 max-w-7xl mx-auto mobile:px-3">
       <div className="text-center">
-        <h3 className="text-xl">{smallTitle}</h3>
-        <h2 className="text-5xl font-semibold">{title}</h2>
+        <h3 className="text-xl tablet:text-lg mobile:text-lg">{smallTitle}</h3>
+        <h2 className="text-5xl font-semibold tablet:text-4xl mobile:text-2xl">
+          {title}
+        </h2>
       </div>
-      <div className="grid mt-5  grid-cols-3 max-w-7xl m-auto">
+      <div className="grid mt-5 grid-cols-3 max-w-7xl m-auto mobile:grid-cols-1">
         {products.map((pr) => (
           <CardProduct product={pr} key={pr.id} />
         ))}
