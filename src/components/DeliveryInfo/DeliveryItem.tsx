ea0fa9b33743005587dee6a065ec9cebe0434ec2
leobar37 <leonbarbozaelmerjoselito@gmail.com>
2023-10-10 12:18:26 -0500

    feat: about coffe info mobile

--- src/components/DeliveryInfo/DeliveryItem.tsx
+++ src/components/DeliveryInfo/DeliveryItem.tsx
@@ -1,3 +1,4 @@
+import NextImage from "next/image";
 interface DeliveryItemProps {
   src: string;
   alt: string;
@@ -6,9 +7,13 @@ interface DeliveryItemProps {
 
 export const DeliveryItem = ({ src, alt, text }: DeliveryItemProps) => {
   return (
-    <div className="flex flex-col items-center">
-      <img className="h-[170px] w-auto" src={src} alt={alt} />
-      <h4 className="font-bold text-3xl mt-4">{text}</h4>
+    <div className="flex flex-col items-center px-1 mobile:py-2">
+      <div className="h-[170px] flex justify-center items-center  tablet:w-28 mobile:h-[170px]">
+        <NextImage src={src} width={170} height={170} alt={alt} />
+      </div>
+      <h4 className="font-bold text-3xl mt-4 tablet:text-xl toTable:text-center mobile:text-xl">
+        {text}
+      </h4>
     </div>
   );
 };
--- src/components/footer/MainFooter.tsx
+++ src/components/footer/MainFooter.tsx
@@ -1,9 +1,10 @@
+import Logo from "~/components/logo";
 const MainFooter = () => {
   return (
     <div className="bg-capsil-brown pt-24 pb-16">
-      <div className="flex justify-between items-center w-full px-36">
+      <div className="flex justify-between items-center w-full px-36 toTable:flex-col tablet:gap-y-4 mobile:gap-y-3">
         <div>
-          <img src="/footer-logo.svg" />
+          <Logo size="lg" variant="green" />
           <div className="flex justify-center space-x-3 m-auto mt-3">
             <a href="#">
               <img src="/Facebook.svg" />
@@ -16,20 +17,20 @@ const MainFooter = () => {
             </a>
           </div>
         </div>
-        <div>
-          <h5 className="font-cormorat text-white text-3xl font-bold">
+        <div className="toTable:text-center ">
+          <h5 className="font-cormorat text-white text-3xl font-bold tablet:text-2xl">
             Contacto
           </h5>
-          <h5 className="mt-11 font-dmSans text-capsil-gold text-sm">
+          <h5 className="mt-11 toTable:mt-2 font-dmSans text-capsil-gold text-sm">
             CONTACTANOS
           </h5>
-          <div className="mt-8 font-dmSans text-[#888682]">
+          <div className="mt-8 font-dmSans toTable:mt-4 text-[#888682]">
             <p>Phone +519293969</p>
             <p>leonbarbozaelmerjoselito@gmail.com</p>
           </div>
         </div>
       </div>
-      <div className="w-full px-16 max-w-6xl m-auto mt-16">
+      <div className="w-full px-16 max-w-6xl m-auto mt-16 toTable:mt-8">
         <div className="w-full border-t border-solid border-[rgba(97,119,152,0.2)]"></div>
         <p className="font-dmSans text-white w-full text-center mt-10">
           © 2022 Capsil Company. All rights reserved.{" "}
